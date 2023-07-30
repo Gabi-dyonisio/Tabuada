4abfba5ff10a0d93d2a27b58b68f94d05fd5d456
Gabi-dyonisio <gabidadyonisio284@gmail.com>
2023-07-30 19:47:58 -0300

    tabuada.html
    aqui está o HTML da tabuada

---
+++ exercício tabuada.js
@@ -0,0 +1,5 @@
+let numero=Number(prompt(`Digite um número qualquer que irá aparecer a tabuada dele`));
+for(i=0;i<=10; i++){
+multiplicacao= i*numero;
+console.log(`${numero} x ${i} = ${multiplicacao}`);
+}
